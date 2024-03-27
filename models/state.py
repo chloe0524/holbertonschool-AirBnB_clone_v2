95a3399bbe991946c7f665aa80013d3e9f4a5b71
GillesR76 <gilles-richard76@hotmail.com>
2024-03-27 15:52:25 +0100

    Refactor BaseModel and add City and State models

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from engine.file_storage import FileStorage
+from models.city import City
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete', backref='state')
+
+    @property
+    def cities(self):
+        city_list = []
+        for city in FileStorage.all(City):
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
