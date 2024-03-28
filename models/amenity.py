7f341415c2d24785adcfe2249cd9ada492528fd3
GillesR76 <gilles-richard76@hotmail.com>
2024-03-28 11:09:05 +0100

    moving back to the original version

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Table, Column, Integer, ForeignKey, String
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """Many-to-many amenity"""
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity,
-                                   back_populates="amenities")
+class Amenity(BaseModel):
+    name = ""
