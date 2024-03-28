5a7221985c622e938b78c05013c62366ad1b5b4c
GillesR76 <gilles-richard76@hotmail.com>
2024-03-28 11:14:40 +0100

    removing the relationship lines for task 7

--- models/user.py
+++ models/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column
 from sqlalchemy import String
@@ -15,6 +17,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    reviews = relationship('Review', cascade='all, delete', backref='user')
-    places = relationship('Place', cascade='all, delete', backref='user')
