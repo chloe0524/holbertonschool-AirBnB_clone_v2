3756e20e01f8780c75c278b72a0f15e9432693d0
chloe0524 <chloco31@gmail.com>
2024-04-08 17:20:54 +0000

    fixed circular import

--- models/state.py
+++ models/state.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 
-
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from models.city import City  # Import the City class
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
-from models import FileStorage
-from models.city import City
 
 
 class State(BaseModel, Base):
-    """ State class + inheritance """
+    """ State class """
     __tablename__ = 'states'
 
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete', backref='state')
+    cities = relationship(
+        'City', back_populates='state', cascade='all, delete')
 
     @property
     def cities(self):
+        from models import storage
         city_list = []
-        for city in FileStorage.all(City):
+        for city in storage.all(City).values():
             if city.state_id == self.id:
                 city_list.append(city)
         return city_list
