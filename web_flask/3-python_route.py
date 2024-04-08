f2cf7c477f50809e8fd0995dd1a1c4bb65c8cb30
chloe0524 <chloco31@gmail.com>
2024-04-08 11:48:59 +0200

    define new route message with value + text attached to it

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""define new route message with value + text attached to it"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hey_route():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hey_route_2():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text_route(text):
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def python_route(text):
+    text = "is cool"
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
