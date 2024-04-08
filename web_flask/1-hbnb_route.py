ed90831cf996300ef7ddaf3f8b5c405b724f56e8
chloe0524 <chloco31@gmail.com>
2024-04-08 10:50:14 +0200

    fixed app.route statement

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,10 +4,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+strict_slashes=False
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def hey_route():
-    return "<p>Hello HBNB!</p>"
+    return "<p>Hello HBNB!</p>", '/'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
