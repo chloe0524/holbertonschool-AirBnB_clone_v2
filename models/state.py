566107f7ca6242f5179c1cb2eced9860ed22cacc
chloe0524 <chloco31@gmail.com>
2024-04-08 17:52:32 +0000

    pycodestyle fix + removed import in method

--- models/state.py
+++ models/state.py
@@ -2,25 +2,23 @@
 """ State Module for HBNB project """
 
 from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
+from models.city import City
+from sqlalchemy import Column, Integer, String, DateTime, func
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship(
-        'City', back_populates='state', cascade='all, delete')
+    city_names = relationship('City', cascade='all, delete', backref='state')
 
     @property
     def cities(self):
-        from models import storage
         city_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
+        for city in self.city_names:
+            if city.state.id == self.id:
                 city_list.append(city)
         return city_list
