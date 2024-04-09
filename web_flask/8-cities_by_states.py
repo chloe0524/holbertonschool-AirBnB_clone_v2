d9c1c7c2ab1d7f5c22fb667063c0054ff6531f19
chloe0524 <127857895+chloe0524@users.noreply.github.com>
2024-04-09 11:22:01 +0200

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -17,13 +17,12 @@ def teardown(exception):
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Display a HTML page with a list of all State objects in DBStorage."""
+    """list of State objects in db"""
     states = storage.all(State).values()
     cities = storage.all(City).values()
 
     return render_template("8-cities_by_states.html",
-                           cities=cities,
-                           states=states)
+                           cities=cities, states=states)
 
 
 if __name__ == "__main__":
