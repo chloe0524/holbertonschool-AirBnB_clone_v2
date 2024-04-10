a1c93bcba16cba39f55a88bae3bf3a8a99b190fa
chloe0524 <chloco31@gmail.com>
2024-04-10 12:15:09 +0000

    put back initial position

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,17 +7,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def teardown(exception):
+    """fghjkl;"""
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ list of all State in db"""
     states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
-
-
-@app.teardown_appcontext
-def teardown(exception):
-    """fghjkl;"""
-    storage.close()
 
 
 if __name__ == "__main__":
