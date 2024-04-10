4731daa42eb2c103b752f64217fe0154d9dc699f
chloe0524 <chloco31@gmail.com>
2024-04-10 11:43:23 +0000

    changed elements position

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,17 +8,17 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    """fghjkl;"""
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ list of all State in db"""
     states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """fghjkl;"""
+    storage.close()
 
 
 if __name__ == "__main__":
